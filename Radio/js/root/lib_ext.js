cccf26892525c49b4f904904b328fdc8776dd3e7
Unknown <ammar.ahmad1993@gmail.com>
2017-11-16 23:25:20 +0500

    add the ability to play music

--- Radio/js/root/lib_ext.js
+++ Radio/js/root/lib_ext.js
@@ -1,10 +1,15 @@
 
-function loadMusicDoc(url, title) {
+function loadMusicDoc(url, title, present_id) {
   var xmlhttp = new XMLHttpRequest();
   xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("data").innerHTML = this.responseText; //debug line
-      document.getElementById("result").innerHTML = init_Menu.parse(this.responseText); //init menu needs to be moved to Menus_Menu
+      if (present_id != 'null') {
+        //if channel reached
+        document.getElementById('mplayer').src = 'http://tunein.com/embed/player/{channel_id}?autoplay=true'.replace('{channel_id}', present_id);      
+      } else {
+        document.getElementById("data").innerHTML = this.responseText; //debug line
+        document.getElementById("result").innerHTML = init_Menu.parse(this.responseText); //init menu needs to be moved to Menus_Menu                  
+      }
     }
   };
   /*
@@ -15,14 +20,14 @@ function loadMusicDoc(url, title) {
 
   }
   */
-  var tmpl = "https://query.yahooapis.com/v1/public/yql?q=select URL, text from xml where url='{_DATA_URL_}' and itemPath='//outline'&diagnostics=false";
+  var tmpl = "https://query.yahooapis.com/v1/public/yql?q=select URL, text, preset_id from xml where url='{_DATA_URL_}' and itemPath='//outline'&diagnostics=false";
   tmpl = tmpl.replace('{_DATA_URL_}', url);
   xmlhttp.open("GET",tmpl, true);
 
   xmlhttp.send();
 }
 
-
+/*
 var Music_Menu = {
 
     item_template: '<li><img width=64 height=64 src="data:image/png;base64,{XMLURL}"> <a href="{HTMLURL}">{TITLE}</a></li>\n',
@@ -60,3 +65,4 @@ var Music_Menu = {
     }
 
 }
+*/
--- Radio/js/root/lib_init.js
+++ Radio/js/root/lib_init.js
@@ -7,7 +7,7 @@ function loadInitDoc() {
       document.getElementById("result").innerHTML = init_Menu.parse(this.responseText);
     }
   };
-  xmlhttp.open("GET", "https://query.yahooapis.com/v1/public/yql?q=select URL, text from xml where url='http://api.radiotime.com/Browse.ashx' and itemPath='//outline'&diagnostics=false", true);
+  xmlhttp.open("GET", "https://query.yahooapis.com/v1/public/yql?q=select URL, text, preset_id from xml where url='http://api.radiotime.com/Browse.ashx' and itemPath='//outline'&diagnostics=false", true);
   xmlhttp.send();
 }
 
@@ -15,7 +15,7 @@ var init_Menu = {
 //<button onclick="javascript:loadMusicDoc('{HTMLURL}','{TITLE}');">hi</button>
 
     //item_template: "<li><img onclick=\"loadMusicDoc('{PATH}','{TEXT}');\" width=64 height=64 src='data:image/png;base64,{XMLURL}'></img> <a href='{HTMLURL}'>{TITLE}</a></li>\n",
-    item_template: "<div class='col-sm-6 col-md-6 col-lg-3'><br><div class='card' style='width:200px'><img class='card-img-top' onclick=\"loadMusicDoc('{PATH}','{TEXT}');\" src='data:image/png;base64,{XMLURL}' alt='Card image' style='width:100%'></img><div class='card-body'>     <a href=''>{TITLE}</a>   </div></div><br></div>\n",
+    item_template: "<div class='col-sm-6 col-md-6 col-lg-3'><br><div class='card' style='width:200px'><img class='card-img-top' onclick=\"loadMusicDoc('{PATH}','{TEXT}','{PRESENT_ID}');\" src='data:image/png;base64,{XMLURL}' alt='Card image' style='width:100%'></img><div class='card-body'>     <a href=''>{TITLE}</a>   </div></div><br></div>\n",
     parse: function(opml) {
 
         var doc;
@@ -40,9 +40,14 @@ var init_Menu = {
                 title   = curr.getAttribute('text');
                 text   = curr.getAttribute('text');
                 htmlurl = curr.getAttribute('URL');
+                preset_id = curr.getAttribute('preset_id');
                 xmlurl  = new Identicon(md5(title), 420).toString();
                 path    = encodeURIComponent(htmlurl); 
-                html += this.item_template.replace(/{TITLE}/, title).replace(/{HTMLURL}/, htmlurl).replace(/{XMLURL}/, xmlurl).replace(/{PATH}/, path).replace(/{TEXT}/, text);
+                if( htmlurl ) {
+                    //alert(String(preset_id));
+                    html += this.item_template.replace(/{TITLE}/, title).replace(/{HTMLURL}/, htmlurl).replace(/{XMLURL}/, xmlurl).replace(/{PATH}/, path).replace(/{TEXT}/, text).replace(/{PRESENT_ID}/, preset_id);
+                }  
+                
             }
 
         }
